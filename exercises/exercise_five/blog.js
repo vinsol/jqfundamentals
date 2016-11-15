b195ccccd6ad742b7aadb5ed892a944a11107e11
gitsegun <segun@dealdey.com>
2016-11-15 13:27:40 +0100

    update to blog.js

---
+++ exercises/exercise_five/blog.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class RevealHiddenText {
+  constructor($blog) {
+    this.$blog = $blog;
+    this.init();
+  }
+
+  init() {
+    this.revealExcerpt(this.$blog);
+  }
+
+  revealExcerpt($blog) {
+   $blog.find('h3').on('click', function(e) {
+      e.preventDefault();
+      const $p = $(this).next();
+
+
+   $p.slideToggle('slow');
+
+      $('p.excerpt')
+        .not($p)
+        .slideUp();
+    });
+  }
+}
+
+$(function() {
+  const $blog = $('#blog');
+  new RevealHiddenText($blog);
+});
