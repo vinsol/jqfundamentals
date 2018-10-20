ea812fe161b0333609e947a613b4c6d8ce9391c0
Shubham Juneja <shubhamjuneja11@gmail.com>
2018-10-20 12:34:09 +0530

    Fixes

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -2,20 +2,20 @@
 $('div.module')
 
 //Come up with three selectors that you could use to get the third item in the #myList unordered list. Which is the best to use? Why?
-$('#myList>li:nth-child(3)')
-$('#myList>li:eq(2)') // is this same as next?
-$('#myList>li').eq(2) //This one is better because nth-child will not work if structure is changed. Also mylist>li will work only if li is direct child.
-$('#myList li').eq(2)
+$('#myList>li:nth-child(3)');
+$('#myList>li:eq(2)'); // is this same as next?
+$('#myList>li').eq(2); //This one is better because nth-child will not work if structure is changed. Also mylist>li will work only if li is direct child.
+$('#myList li').eq(2);
 
 
 //Select the label for the search input using an attribute selector.
-$("label[for='q']")
+$("label[for='q']");
 
 //Figure out how many elements on the page are hidden
-$(":hidden").length
+$(":hidden").length;
 
 //Figure out how many image elements on the page have an alt attribute.
-$("img[alt]").length
+$("img[alt]").length;
 
 //Select all of the odd table rows in the table body.
-('tbody>tr:odd')
+('tbody>tr:odd');
